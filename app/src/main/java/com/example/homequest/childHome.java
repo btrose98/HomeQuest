342b8e1e0685df1e562f588072221d3ad3e0edb1
Bradley Rose <rose.bradley9@gmail.com>
2020-11-19 22:15:18 -0500

    Created onclick event from childHome to calendar
    onclick occurs when the calender image button is pressed

--- app/src/main/java/com/example/homequest/childHome.java
+++ app/src/main/java/com/example/homequest/childHome.java
@@ -2,13 +2,31 @@ package com.example.homequest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class childHome extends AppCompatActivity {
+
+    private ImageButton btCalender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_home);
+
+        btCalender = (ImageButton) findViewById(R.id.childCalendar);
+
+        btCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(childHome.this, calendar.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
