cdba5fbbc371ec481cfc71492f5914b7644c2cc3
Jennifer <nguyen_jenn2000@yahoo.ca>
2020-11-23 13:09:01 -0500

    Added more pages

---
+++ app/src/main/java/com/example/homequest/redeemRewards.java
@@ -0,0 +1,14 @@
+package com.example.homequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class redeemRewards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redeem_rewards);
+    }
+}
---
+++ app/src/main/java/com/example/homequest/trackProgress.java
@@ -0,0 +1,14 @@
+package com.example.homequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trackProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_progress);
+    }
+}
