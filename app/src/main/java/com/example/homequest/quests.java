fefb49e29bbaefbab4c7c097dfd432439848ed0b
Bradley Rose <rose.bradley9@gmail.com>
2020-11-23 21:19:27 -0500

    linked back button on quest page

--- app/src/main/java/com/example/homequest/quests.java
+++ app/src/main/java/com/example/homequest/quests.java
@@ -3,12 +3,26 @@ package com.example.homequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class quests extends AppCompatActivity {
+
+    private ImageButton backButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quests);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
