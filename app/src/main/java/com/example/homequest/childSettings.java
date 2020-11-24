0e2888e4901e74a6afecb198dcf08f66df085466
Bradley Rose <rose.bradley9@gmail.com>
2020-11-23 21:29:43 -0500

    linked back buttons on rewards and settings page for child view

--- app/src/main/java/com/example/homequest/childSettings.java
+++ app/src/main/java/com/example/homequest/childSettings.java
@@ -3,12 +3,25 @@ package com.example.homequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class childSettings extends AppCompatActivity {
+
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_settings);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/homequest/redeemRewards.java
+++ app/src/main/java/com/example/homequest/redeemRewards.java
@@ -3,12 +3,25 @@ package com.example.homequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class redeemRewards extends AppCompatActivity {
+
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redeem_rewards);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
