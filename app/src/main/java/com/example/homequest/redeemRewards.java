dc7404234dfa410bd7574c87394797d046ae5900
Bradley Rose <rose.bradley9@gmail.com>
2020-11-29 11:40:44 -0500

    trying to create unlock functionality in Redeem Rewards
    ERROR - Unable to open Redeem Reward screen from childHome
    
    unlock functionality added in Redeem Reward line 36

--- app/src/main/java/com/example/homequest/redeemRewards.java
+++ app/src/main/java/com/example/homequest/redeemRewards.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -12,6 +13,7 @@ public class redeemRewards extends AppCompatActivity {
 
     private ImageButton backButton;
     private Button btReward;
+    private static CheckBox taskComplete1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,9 @@ public class redeemRewards extends AppCompatActivity {
         setContentView(R.layout.activity_redeem_rewards);
 
         backButton = findViewById(R.id.backButton);
-        btReward = (Button) findViewById(R.id.btReward1);
+        btReward = findViewById(R.id.btReward1);
+        taskComplete1 = findViewById(R.id.btTrackProg1);
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,11 +32,20 @@ public class redeemRewards extends AppCompatActivity {
             }
         });
 
+
+        if(taskComplete1.isChecked()){
+            btReward.setText("Sleepover");
+        }else{
+            btReward.setText("Locked");
+        }
+
         btReward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(redeemRewards.this, "Reward Claimed: 1 sleepover!", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 }
