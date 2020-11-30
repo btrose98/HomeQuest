4a558fc60f5f1ce05ab290ecc791f50c399a2397
Bradley Rose <rose.bradley9@gmail.com>
2020-11-30 15:17:42 -0500

    Assigning a chore clears description fields and creates toast saying chore assigned

--- app/src/main/java/com/example/homequest/assignChoresnew.java
+++ app/src/main/java/com/example/homequest/assignChoresnew.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class assignChoresnew extends AppCompatActivity {
 
     private ImageButton backButton;
+    private Button submit;
+    EditText choreDescr, rewardDescr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,9 @@ public class assignChoresnew extends AppCompatActivity {
 
 
         backButton = findViewById(R.id.backButton1);
+        submit = findViewById(R.id.btAssignChore);
+        choreDescr = findViewById(R.id.choreDescription);
+        rewardDescr = findViewById(R.id.rewardDescription);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +33,18 @@ public class assignChoresnew extends AppCompatActivity {
             }
         });
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(choreDescr.getText().toString().equals("") || rewardDescr.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "You must fill in all of the above information to assign this chore.",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Chore has been assigned.",Toast.LENGTH_SHORT).show();
+                    choreDescr.setText("");
+                    rewardDescr.setText("");
+                }
+            }
+        });
 
 
 
