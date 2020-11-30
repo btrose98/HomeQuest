7355054ac104a7cb6c6d35a3292f9c46daa31b89
Bradley Rose <rose.bradley9@gmail.com>
2020-11-30 15:33:22 -0500

    Added Toast messages for specific use cases in parentSettings

--- app/src/main/java/com/example/homequest/parentSettings.java
+++ app/src/main/java/com/example/homequest/parentSettings.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class parentSettings extends AppCompatActivity {
 
     private ImageButton backButton;
+    Button changePassword, addChild;
+    EditText oldPassword, newPassword, addChildName, addChildPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,12 @@ public class parentSettings extends AppCompatActivity {
         setContentView(R.layout.activity_parent_settings);
 
         backButton = findViewById(R.id.backButton);
+        changePassword = findViewById(R.id.btChangePassword);
+        addChild = findViewById(R.id.btAddChild);
+        oldPassword = findViewById(R.id.oldPassword);
+        newPassword = findViewById(R.id.newPassword);
+        addChildName = findViewById(R.id.addChildName);
+        addChildPassword = findViewById(R.id.addChildPassword);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +36,34 @@ public class parentSettings extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(oldPassword.getText().toString().equals("") || newPassword.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Please fill in all of the above fields to complete this action.",Toast.LENGTH_SHORT).show();
+                }else if(oldPassword.getText().toString().equals(newPassword.getText().toString())){
+                    Toast.makeText(getApplicationContext(), "New password must be different from the old password.",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Password has been changed.",Toast.LENGTH_SHORT).show();
+                    oldPassword.setText("");
+                    newPassword.setText("");
+                }
+            }
+        });
+
+        addChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(addChildName.getText().toString().equals("") || addChildPassword.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Please fill in all of the above fields to complete this action.",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Child has been added to your home.",Toast.LENGTH_SHORT).show();
+                    addChildName.setText("");
+                    addChildPassword.setText("");
+                }
+            }
+        });
+
     }
 }
